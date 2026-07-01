ffb0a50355d0c9a075ef7e713232da358374b3af
Spetes <spetesq@gmail.com>
2026-07-01 10:21:27 +0300

    almost final

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,41 @@ use alloc::vec;
 use alloc::vec::Vec;
 use libm::powf;
 
+#[cfg(feature = "bincode")]
+use bincode_next::{Decode, Encode};
+
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+/// Gerder for user data
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub enum Gender {
     Female,
     Male,
     None,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+///Custom struct for data ( i wanted to use chrono but no std doesnt support it)
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub struct DateTime {
     pub year: i32,
     pub month: u8,
     pub day: u8,
     pub hour: u8,
     pub minute: u8,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
+pub enum Unit {
+    #[default]
+    Metric,
+    Imperial,
 }
 
 impl DateTime {
@@ -35,20 +56,30 @@ impl DateTime {
     }
 }
 
+/// WeightEntry consider weight and time for graphs (im not sure that DataTime is the best way)
 #[derive(Debug, Clone, Copy, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub struct WeightEntry {
     pub time: Option<DateTime>,
     pub weight: f32,
 }
 
+/// UserData for gender, age, height
 #[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub struct UserData {
     pub gender: Gender,
     pub age: u8,
     pub height: f32,
+    pub unit: Unit,
 }
 
+/// User consider UserData and Vec of WeightEntry
 #[derive(Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub struct User {
     pub data: UserData,
     weight: Vec<WeightEntry>,
@@ -59,6 +90,7 @@ impl User {
         age: u8,
         height: f32,
         weight: f32,
+        unit: Option<Unit>,
         time_point: Option<DateTime>,
     ) -> Result<Self, UserError> {
         if weight < 10.0 {
@@ -70,6 +102,7 @@ impl User {
             gender,
             age,
             height,
+            unit: unit.unwrap_or_default(),
         };
         let weight_entry = WeightEntry {
             time: time_point,
@@ -80,25 +113,57 @@ impl User {
             weight: vec![weight_entry],
         })
     }
-    pub fn change_user_data(&mut self, data: UserData) {
+    /// Fn to change user data like height, age etc
+    pub fn change_user_data(&mut self, data: UserData) -> Result<(), UserError> {
+        if data.height < 30.0 {
+            return Err(UserError::WrongHeight);
+        }
         self.data = data;
+        Ok(())
     }
-    pub fn get_last_weight(&self) -> Option<f32> {
-        self.weight.last().map(|weight_entry| weight_entry.weight)
+    /// Fn to get last height I advise you to use the function of this structure, because otherwise there may be an error
+    pub fn get_last_weight(&self) -> f32 {
+        unsafe { self.weight.get_unchecked(self.weight.len() - 1).weight }
     }
-    pub fn add_weight(&mut self, weight: f32, time_point: Option<DateTime>) {
+    pub fn add_weight(
+        &mut self,
+        weight: f32,
+        time_point: Option<DateTime>,
+    ) -> Result<(), UserError> {
+        if weight < 10.0 {
+            return Err(UserError::WrongWeight);
+        }
         let weight_entry = WeightEntry {
             time: time_point,
             weight,
         };
         self.weight.push(weight_entry);
+        Ok(())
+    }
+    pub fn change_weight(&mut self, id: usize, weight_entry: WeightEntry) -> Result<(), UserError> {
+        if let Some(entry) = self.weight.get_mut(id) {
+            if weight_entry.weight < 10.0 {
+                return Err(UserError::WrongWeight);
+            }
+            *entry = weight_entry;
+            return Ok(());
+        }
+        Err(UserError::NotFound)
+    }
+    /// Fn to get history of weight
+    pub fn weight_history(&self, amount: Option<usize>) -> Vec<WeightEntry> {
+        let am = amount.unwrap_or(self.weight.len());
+        let start_index = self.weight.len().saturating_sub(am);
+        self.weight[start_index..].to_vec()
     }
 
     pub fn get_health_report(&self) -> HealthReport {
-        let weight = self.get_last_weight().expect("failed to get weight");
-        let height = self.data.height;
+        let (weight, height) = match self.data.unit {
+            Unit::Metric => (self.get_last_weight(), self.data.height),
+            Unit::Imperial => (self.get_last_weight() * 0.45359237, self.data.height * 2.54),
+        };
 
-        let actual_bmi = calculate_bmi(weight, height);
+        let bmi = calculate_bmi(weight, height);
         let age = self.data.age;
 
         let age_offset = match age {
@@ -110,7 +175,7 @@ impl User {
             _ => 24.0,
         };
 
-        let target_weight = match self.data.gender {
+        let target_weight_kg = match self.data.gender {
             Gender::Male => (height - 100.0) - ((height - 150.0) / 4.0),
             Gender::Female => (height - 100.0) - ((height - 150.0) / 2.5),
             Gender::None => {
@@ -120,9 +185,20 @@ impl User {
             }
         };
 
+        let target_weight = match self.data.unit {
+            Unit::Metric => target_weight_kg,
+            Unit::Imperial => target_weight_kg / 0.45359237,
+        };
+
         let base_norm = 19.0;
-        let bmi = actual_bmi - (age_offset - base_norm);
-        let extra_weight = weight - target_weight;
+        let actual_bmi = bmi - (age_offset - base_norm);
+        let extra_weight = match self.data.unit {
+            Unit::Metric => weight - target_weight_kg,
+            Unit::Imperial => {
+                let extra_kg = weight - target_weight_kg;
+                extra_kg / 0.45359237
+            }
+        };
 
         let min_normal = age_offset;
         let max_normal = min_normal + 6.0;
@@ -154,17 +230,29 @@ pub fn calculate_bmi(weight: f32, height: f32) -> f32 {
     1.3 * weight / powf(height / 100.0, 2.5)
 }
 
+/// A small report for health
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub struct HealthReport {
+    /// Just bmi according to the Oxford formula
     pub bmi: f32,
+    /// The best height for user
     pub target_weight: f32,
+    /// BMI varies slightly with age
     pub age_offset: f32,
+    /// BMI with age offset
     pub actual_bmi: f32,
+    /// Amount of excess weight (kg)
     pub extra_weight: f32,
+    /// Weight status: underweight, normal weight, etc.
     pub status: Status,
 }
 
+/// Weight status: underweight, normal weight, etc.
 #[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "bincode", derive(Encode, Decode))]
 pub enum Status {
     SevereWeightDeficit,
     Underweight,
@@ -172,12 +260,9 @@ pub enum Status {
     Overweight,
     Obesity,
 }
-pub enum HealthError {
-    NoWeight,
-    InvalidHeight,
-}
 #[derive(Debug)]
 pub enum UserError {
     WrongWeight,
     WrongHeight,
+    NotFound,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use BMI_calculator::{HealthReport, User};
+use BMI_calculator::{HealthReport, Unit, User};
 
 fn main() {
-    let user = User::new(BMI_calculator::Gender::Male, 16, 175.0, 78.35, None).unwrap();
+    let mut user = User::new(BMI_calculator::Gender::Male, 75, 175.0, 78.35, None, None).unwrap();
+    user.add_weight(70.0, None);
+    user.add_weight(99.0, None);
+    user.add_weight(65.0, None);
     let r = user.get_health_report();
-    println!("{:?}", r)
+    println!("{:?}", r);
+    println!("{:?}", user.weight_history(None));
 }
